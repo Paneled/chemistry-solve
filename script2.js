f30ce5d0714835221773659fc507571bcd1c109e
Paneled <78773149+Paneled@users.noreply.github.com>
2023-02-17 23:48:42 -0500

    h

--- script2.js
+++ script2.js
@@ -3,7 +3,7 @@ function parsePercents(){
     var actual = document.getElementById("final").value;
     var doSub = exp-actual;
     var doFrac = (Math.abs(doSub/actual));
-    var out = (Math.round((doFrac*100)*100)/100) + " %";
+    var out = (Math.round((doFrac*100)*100)/100) + " % error";
 
     document.getElementById("output").innerHTML = out;
 }
