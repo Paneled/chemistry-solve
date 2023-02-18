ddaae866f19334acb9d6eca0da2fdf00eb270071
Paneled <78773149+Paneled@users.noreply.github.com>
2023-02-17 23:44:49 -0500

    rework everything

---
+++ script2.js
@@ -0,0 +1,9 @@
+function parsePercents(){
+    var exp = document.getElementById("exp").value;
+    var actual = document.getElementById("final").value;
+    var doSub = exp-actual;
+    var doFrac = (Math.abs(doSub/actual));
+    var out = (Math.round((doFrac*100)*100)/100) + " %";
+
+    document.getElementById("output").innerHTML = out;
+}
